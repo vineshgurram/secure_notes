e873032ffee6f638c884dffa07ebc7616c3e0bd8
Vinesh <vinesh.gurram@liqvd.asia>
2026-02-04 18:28:45 +0530

    updated db details

--- config/config.php
+++ config/config.php
@@ -2,7 +2,8 @@
 
 // $host = "localhost";
 $host = "sql210.infinityfree.com";
-$dbname = "secure_notes";
+// $dbname = "secure_notes";
+$dbname = "if0_41059758_secure_notes";
 // $user = "root";
 $user = "if0_41059758";
 // $pass = "";
